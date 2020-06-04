3c2f807f7b3194c64eea1aa17320d709d8912bed
Rishik Hombal <Rishik.Hombal@altec.com>
2020-06-04 15:51:19 -0500

    Fixed module 3

--- src/app/album.ts
+++ src/app/album.ts
@@ -1,6 +1,6 @@
 import { Track } from "./track";
 
 export interface Album {
-  name, releaseDate, coverImage: string;
+  name: string, releaseDate: string, coverImage: string;
   tracks: Track[];
 }
